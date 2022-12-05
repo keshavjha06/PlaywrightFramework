56c8465633373afde4fffbb726a7c5ce4daa8c2a
Keshav Jha <keshavjha0610@gmail.com>
2022-12-05 23:43:07 +0530

    updated vaildations.spec.js

--- tests/validations.spec.js
+++ tests/validations.spec.js
@@ -16,3 +16,20 @@ test("popup validations", async ({ page }) => {
   const textCheck = await framesPage.locator(".text h2").textContent();
   console.log(textCheck.split(" ")[1]);
 });
+
+test("Screenshot & Visual comparision", async ({ page }) => {
+  await page.goto("https://rahulshettyacademy.com/AutomationPractice/");
+  await expect(page.locator("#displayed-text")).toBeVisible();
+  await page
+    .locator("#displayed-text")
+    .screenshot({ path: "partialScreenshot.png" });
+  await page.locator("#hide-textbox").click();
+  await page.screenshot({ path: "screenshot.png" });
+  await expect(page.locator("#displayed-text")).toBeHidden();
+});
+//screenshot -store -> screenshot ->
+test("visual", async ({ page }) => {
+  //make payment -when you 0 balance
+  await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+  expect(await page.screenshot()).toMatchSnapshot("landing.png");
+});
