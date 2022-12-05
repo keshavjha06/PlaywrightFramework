4dde2129fae6935fa7961b4850ccc2ab379c2cbd
Keshav Jha <keshavjha0610@gmail.com>
2022-12-05 23:42:53 +0530

    updated basictest.spec.js

--- tests/basictest.spec.js
+++ tests/basictest.spec.js
@@ -1,6 +1,6 @@
 const { test, expect } = require("@playwright/test");
 
-test.only("browser context playwright test", async ({ browser }) => {
+test("browser context playwright test", async ({ browser }) => {
   const context = await browser.newContext();
   const page = await context.newPage();
   page.route("**/*.css", (route) => route.abort());
