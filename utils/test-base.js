76bf89e8f6d5eac488c3f7d0b15fb4bdc008f259
Keshav Jha <keshavjha0610@gmail.com>
2022-12-06 11:29:24 +0530

    modified test-base.js

--- utils/test-base.js
+++ utils/test-base.js
@@ -4,6 +4,6 @@ exports.customtest = base.test.extend({
   testDataForOrder: {
     username: "anshika@gmail.com",
     password: "Iamking@000",
-    productName: "Zara Coat 4",
+    productName: "zara coat 3",
   },
 });
