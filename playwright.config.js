bf2ef83f0040166ec8e3fa0fae25f1ba0ddef113
Keshav <keshavjha0610@gmail.com>
2026-05-18 18:14:49 +0530

    feat: integrate Microsoft Playwright Service, add Azure dependencies, and update configuration files

--- playwright.config.js
+++ playwright.config.js
@@ -1,22 +1,32 @@
 // @ts-check
-import { devices } from "@playwright/test";
+const { devices } = require('@playwright/test');
 
 const config = {
-  testDir: "./tests",
+  testDir: './tests',
+  testMatch: '**/*.spec.js',
   retries: 0,
+
   /* Maximum time one test can run for. */
   timeout: 30 * 1000,
   expect: {
-    timeout: 5000,
+
+    timeout: 5000
   },
-  reporter: "html",
+
+  reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    browserName: "chromium",
+
+    browserName: 'chromium',
     headless: true,
-    screenshot: "on",
-    trace: "retain-on-failure", //off,on
+    screenshot: 'on',
+    trace: 'on',//off,on
+
+
+
   },
+
+
 };
 
 module.exports = config;
